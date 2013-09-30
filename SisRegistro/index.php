044d9ae4907f24b86770e1a6c4abedce7821dabb
Jack <Jack@Jack-VAIO>
2013-09-30 12:00:02 -0500

    Titulo

---
+++ SisRegistro/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Base de Datos</title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>

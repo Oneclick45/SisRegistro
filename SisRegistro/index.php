88b11fc9e76b0b31ecff77fa1d6eaeb579f08d43
Oneclick45 <braddy.300.iu@gmail.com>
2013-09-30 12:06:21 -0500

    Creando conflicto
    por motivos de apagón

--- SisRegistro/index.php
+++ SisRegistro/index.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Base de Datos</title>
+        <title>UNA-Puno</title>
     </head>
     <body>
     <ul>
